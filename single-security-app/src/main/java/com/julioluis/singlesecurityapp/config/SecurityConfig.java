03b7b1ebda20b981afc63170f20731161119d378
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-04-21 19:36:27 -0400

    Authentication using token

--- single-security-app/src/main/java/com/julioluis/singlesecurityapp/config/SecurityConfig.java
+++ single-security-app/src/main/java/com/julioluis/singlesecurityapp/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.julioluis.singlesecurityapp.config;
 
 
+import com.julioluis.singlesecurityapp.filter.JWTTokenGeneratorFilter;
+import com.julioluis.singlesecurityapp.filter.JWTTokenValidatorFilter;
+import com.julioluis.singlesecurityapp.filter.RequestValidationBeforeFilter;
 import com.julioluis.singlesecurityapp.filters.CsrfCookiFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 
@@ -26,8 +30,8 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler requestHander=new CsrfTokenRequestAttributeHandler();
         requestHander.setCsrfRequestAttributeName("_csrf");
-        http.securityContext().requireExplicitSave(false)
-                .and().sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .cors().configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -36,6 +40,7 @@ public class SecurityConfig {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
                         config.setMaxAge(3600L);
 
                         return config;
@@ -45,12 +50,10 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHander).ignoringRequestMatchers("/contact","/register")
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())).
                 addFilterAfter(new CsrfCookiFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new RequestValidationBeforeFilter(),BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(),BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(),BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
-//                .requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
-//                .requestMatchers("/myBalance").hasAnyAuthority("VIEWACCOUNT","VIEWBALANCE")
-//                .requestMatchers("/myCards").hasAuthority("VIEWCARDS")
-//                .requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
-
                 .requestMatchers("/myAccount").hasRole("USER")
                 .requestMatchers("/myBalance").hasAnyRole("USER","ADMIN")
                 .requestMatchers("/myCards").hasRole("USER")
